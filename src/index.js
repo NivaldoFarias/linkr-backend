2913ac844e09bd282a2b7a584017fdf8d4bb3457
Nivaldo Farias <badivia.f@gmail.com>
2022-06-15 11:59:26 -0300

    chore: add nullish coalescing to index for heroku deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from './app.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const port = process.env.PORT;
+const port = process.env.PORT ?? 5000;
 app.listen(port, () => {
   console.log(`Server listening on http://localhost:${port}`);
 });
