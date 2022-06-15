7372a12efaac1ccd754b06aec180a091f233331e
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-06-15 00:04:01 -0300

    fix: middleware -> middlewares in app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import routes from "./routes/index.js"
-import ErrorHandler from "./middleware/ErrorHandler.js";
+import ErrorHandler from "./middlewares/ErrorHandler.js";
 
 const app = express();
 
