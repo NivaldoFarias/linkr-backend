a490fa33f1f91f901dda37e698ee85c9b023158d
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-06-15 09:45:15 -0300

    fix: typos on sql queries

--- src/repositories/getPostsByUserId.js
+++ src/repositories/getPostsByUserId.js
@@ -1,12 +1,14 @@
+import db from '../database/index.js';
+
 const getPostsByUserId = async (id) => {
 
     const searchQuery = `
-        SELECT posts.id, posts.text, posts.url, posts.created_at as 'createdAt'
+        SELECT posts.id, posts.text, posts.url, posts.created_at as "createdAt"
         FROM posts
         WHERE posts.user_id = $1;
     `;
-    const posts = await db.query(searchQuery,[id]);
-    return posts;
+    const results = await db.query(searchQuery,[id]);
+    return results.rows;
 };
 
 export default getPostsByUserId;
