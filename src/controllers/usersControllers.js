4f15b2ed1fa3045f7199a51d07293f1b6e6a02e5
Nivaldo Farias <badivia.f@gmail.com>
2022-06-15 11:48:00 -0300

    chore: format controller files to prettier config

--- src/controllers/usersControllers.js
+++ src/controllers/usersControllers.js
@@ -1,21 +1,21 @@
-import { userRepository } from "../repositories/users.js";
+import { userRepository } from '../repositories/users.js';
 
 export const usersByUserName = async (req, res, next) => {
-    try {
-        const { username } = req.params;
-        const users = await userRepository.getUsersByUserName(username);
-        res.status(200).json({ users });
-    } catch (e) {
-        next(e);
-    }
+  try {
+    const { username } = req.params;
+    const users = await userRepository.getUsersByUserName(username);
+    res.status(200).json({ users });
+  } catch (e) {
+    next(e);
+  }
 };
 
 export const retrievePosts = async (req, res, next) => {
-    try {
-        const { userId: id } = req.params;
-        const posts = await userRepository.getPostsByUserId(id);
-        res.status(200).json({ posts });
-    } catch (e) {
-        next(e);
-    }
-}
+  try {
+    const { userId: id } = req.params;
+    const posts = await userRepository.getPostsByUserId(id);
+    res.status(200).json({ posts });
+  } catch (e) {
+    next(e);
+  }
+};
