ab63c697088e4b1540f45eecf337f4e15187910b
Vitor Junior <vitorjunior@poli.ufrj.br>
2022-06-16 00:48:15 -0300

    refactor: improving performance

--- src/repositories/posts.js
+++ src/repositories/posts.js
@@ -14,9 +14,9 @@ async function getPostsByHashtagId(hashtagId) {
 
 async function getPostsByUserId(userId) {
   const query = `
-        SELECT p.*, u.username, u.image_url FROM posts p
-        JOIN users u ON p.user_id = u.id
-        JOIN hashtags_posts hp ON p.id = hp.post_id
+        SELECT p.*, u.username, u.image_url FROM users u
+        JOIN posts p ON p.user_id = u.id
+        LEFT JOIN hashtags_posts hp ON p.id = hp.post_id
         WHERE p.user_id = $1
         ORDER BY p.created_at DESC;
     `;
@@ -24,7 +24,18 @@ async function getPostsByUserId(userId) {
   return response.rows;
 }
 
+async function insertPost(text, url, user_id) {
+  return db.query(
+    `
+        INSERT INTO posts (text, url, user_id)
+        VALUES ($1, $2, $3)
+    `,
+    [text, url, user_id],
+  );
+}
+
 export const postsRepository = {
   getPostsByHashtagId,
   getPostsByUserId,
+  insertPost,
 };
