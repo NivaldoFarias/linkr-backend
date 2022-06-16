ab0411779c4986a35cdc9c22fad787a21e40093a
Nivaldo Farias <badivia.f@gmail.com>
2022-06-15 21:42:57 -0300

    feat: add 'urls' table and foreign key (id) reference to posts

--- database.sql
+++ database.sql
@@ -19,8 +19,20 @@ CREATE TABLE "posts" (
     "url" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "user_id" BIGINT NOT NULL,
+    "url_id" BIGINT NOT NULL,
 
     CONSTRAINT "posts_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "urls" (
+    "id" BIGSERIAL NOT NULL,
+    "url" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
+    "image_url" TEXT NOT NULL,
+
+    CONSTRAINT "urls_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -56,6 +68,9 @@ CREATE UNIQUE INDEX "users_username_key" ON "users"("username");
 
 -- AddForeignKey
 ALTER TABLE "posts" ADD CONSTRAINT "posts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "posts" ADD CONSTRAINT "posts_url_id_fkey" FOREIGN KEY ("url_id") REFERENCES "urls"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "likes" ADD CONSTRAINT "likes_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
