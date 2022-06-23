f30e4a33af7e039b645fb340d8d5ef4b3e07b6ad
Estevam <estevamfurtado@gmail.com>
2022-06-23 15:02:26 -0300

    feat: get user by id route

--- src/routes/usersRoute.js
+++ src/routes/usersRoute.js
@@ -1,7 +1,7 @@
 import { Router } from 'express';
 import logThis from '../blueprints/logThis.js';
 import requireToken from '../middlewares/requireToken.js';
-import { getUser, usersByUserName } from '../controllers/usersController.js';
+import { getUser, getUserById, usersByUserName } from '../controllers/usersController.js';
 import { validateUserId, validateVisitedUserId } from '../middlewares/usersMiddleware.js';
 import { checkGetSharesQuery, checkCheckSharesQuery } from '../middlewares/sharesMiddleware.js';
 import { getUserData, checkUserShares } from '../controllers/feedSharesController.js';
@@ -17,6 +17,14 @@ const usersRoute = Router();
 usersRoute.route('/').get(logThis('Get user'), requireToken, getUser);
 usersRoute.route('/username/:username').get(logThis('Find users'), requireToken, usersByUserName);
 
+usersRoute.get(
+  '/:userId',
+  logThis('Get user by id'),
+  requireToken,
+  validateUserId,
+  validateVisitedUserId,
+  getUserById,
+);
 usersRoute.get(
   '/:userId/posts',
   logThis('Get user shares'),
