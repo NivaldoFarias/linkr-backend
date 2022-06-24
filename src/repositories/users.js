5d674d7f28c0d390208c0070d75d85190a14883c
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-06-24 11:08:08 -0300

    CHORE - searchbar prioriza usuarios já seguidos

--- src/repositories/users.js
+++ src/repositories/users.js
@@ -1,14 +1,22 @@
 import db from '../database/index.js';
 
-const getUsersByUserName = async (username) => {
+const getUsersByUserName = async (username, user_id) => {
+  const name = username.toLowerCase();
   const searchQuery = `
-        SELECT users.id, users.username, users.image_url as "imageUrl"
-        FROM users
-        WHERE LOWER(users.username) LIKE $1
-        ORDER BY users.username ASC;
-    `;
-  const result = await db.query(searchQuery, [`${username.toLowerCase()}%`]);
-  return result.rows;
+    SELECT
+      usr.id,
+      usr.username, 
+      usr.image_url as "imageUrl", 
+      usr.email, 
+      f.user_id as "followerId"
+    FROM users u
+    LEFT JOIN followings f ON f.user_id=u.id
+    LEFT JOIN users usr ON usr.id = f.followed_id
+    WHERE LOWER(usr.username) LIKE $1 AND (u.id = $2 OR u.id!=$2) AND u.id != f.followed_id
+    ORDER BY u.id;
+  `
+  const result = await db.query(searchQuery, [`${name}%`,user_id]);
+  return result.rows ?? null;
 };
 
 const getUserById = async (id) => {
